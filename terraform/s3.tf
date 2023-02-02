7067f5853e2ef58b1b74ec1381b374bf329c088a
diogofiaminghi <diogofiaminghi@gmail.com>
2023-02-02 14:43:40 -0300

    static-website working

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -18,22 +18,12 @@ resource "aws_s3_bucket_website_configuration" "website" {
   bucket = aws_s3_bucket.website.id
 
   index_document {
-    suffix = "/html5up-hyperspace/index.html"
+    suffix = "index.html"
   }
 
-  //  error_document {
-  //    key = "error.html"
-  //  }
-
-  /*  routing_rule {
-    condition {
-      key_prefix_equals = "docs/"
-    }
-    redirect {
-      replace_key_prefix_with = "documents/"
-    }
+  error_document {
+    key = "error.html"
   }
-*/
 }
 
 resource "aws_s3_bucket_policy" "website" {
@@ -59,14 +49,26 @@ data "aws_iam_policy_document" "allow_public_access" {
   }
 }
 
-resource "aws_s3_bucket_object" "html-directory" {
-  for_each = fileset("../html5up-hyperspace/", "**")
-  bucket   = aws_s3_bucket.website.id
-  key      = each.value
-  source   = "../html5up-hyperspace/${each.value}"
+resource "aws_s3_object" "index_page" {
+  bucket       = aws_s3_bucket.website.id
+  key          = "index.html"
+  content_type = "text/html; charset=UTF-8"
+  source       = "../website/index.html"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
-  etag = filemd5("../html5up-hyperspace/${each.value}")
+  etag = filemd5("../website/index.html")
+}
+
+resource "aws_s3_object" "error_page" {
+  bucket       = aws_s3_bucket.website.id
+  key          = "error.html"
+  content_type = "text/html; charset=UTF-8"
+  source       = "../website/error.html"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("../website/error.html")
 }
